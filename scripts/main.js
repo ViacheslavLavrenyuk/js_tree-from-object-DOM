a2f329fba66224b1ddfcb8462dcfd101573b5a2d
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-02-05 08:02:11 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const item in data) {
+    const li = document.createElement('li');
+
+    li.textContent = item;
+    ul.append(li);
+
+    if (typeof data[item] === 'object') {
+      createTree(li, data[item]);
+    }
+  }
+}
+
+createTree(tree, food);
